11cebae06c755533f20604b6df2465430ee644f9
Federico Mujica <federicomujica1@gmail.com>
2024-05-27 20:08:46 +0200

    CSS details

--- src/rendering/src/components/Navigation/MainNavigation.tsx
+++ src/rendering/src/components/Navigation/MainNavigation.tsx
@@ -90,7 +90,7 @@ const MainNavigation = (props: MainNavigationProps): JSX.Element => {
             {shopLink}
             <li className="button-menu-item">
               <Link href="/tickets" className="btn-main">
-                {t('Contact Us') || 'Contact Us'}
+                {t('Register Now') || 'Register Now'}
               </Link>
             </li>
           </ul>
